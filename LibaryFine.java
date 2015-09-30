9dc68010d6e6c4e46179deeeb103af75b63aaa99
TGuptaNYC <tguptanyc@gmail.com>
2015-09-30 10:09:56 -0400

    Finished program with max min sum and output

--- LibaryFine.java
+++ LibaryFine.java
@@ -30,10 +30,36 @@ public class LibraryFine {
         System.out.println("Enter last name.");
         lastname= input.next();
         // gets last name
+         
+        double[] fine;
+        fine = new double[10];
+        // creates array for fines
+  
+        for(int loop = 0; loop < 10;) {
+            int finenum = loop + 1;
+            System.out.println("Enter " + finenum + "st fine.");
+            fine[loop] = input.nextDouble();
+            loop++;
+        }
+                
+        double maxfine = Math.max( fine[0], Math.max( fine[1], Math.max( fine[2], Math.max( fine[3], Math.max( fine[4], Math.max( fine[5], Math.max( fine[6], Math.max( fine[7], Math.max(fine[8], fine[9])))))))));
+        double minfine = Math.min( fine[0], Math.min( fine[1], Math.min( fine[2], Math.min( fine[3], Math.min( fine[4], Math.min( fine[5], Math.min( fine[6], Math.min( fine[7], Math.min(fine[8], fine[9])))))))));
+        // finds max and min of arrays
         
-        double fine1, fine2, fine3, fine4, fine5, fine6, fine7, fine8, fine9, fine10;
-        double maxfine = Math.max( fine1, Math.max( fine2, Math.max( fine3, Math.max( fine4, Math.max( fine5, Math.max( fine6, Math.max( fine7, Math.max( fine8, Math.max(fine9, fine10)))))))));
+        double sum = fine[0] + fine[1] + fine[2] + fine[3] + fine[4] + fine[5] + fine[6] + fine[7] + fine[8] + fine[9];
+        // finds sum
         
+        String output;
+        output = firstname + " " + middlename + " " + lastname + ", your total fine is $" + sum;
+        JOptionPane.showMessageDialog(null, output);
+        
+        String maxfineoutput = "Your maximum fine is " + maxfine;
+        JOptionPane.showMessageDialog(null, maxfineoutput);
+        
+        String minfineoutput = "Your minimum fine is " + minfine;
+        JOptionPane.showMessageDialog(null, minfineoutput); 
+        
+        // output as expected - 9/30/15
         
     }
     
