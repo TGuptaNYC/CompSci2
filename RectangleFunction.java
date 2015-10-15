6d32c8749df2b9a1c5e29d7b660bfc298508dac9
TGuptaNYC <tguptanyc@gmail.com>
2015-10-14 21:52:07 -0400

    Added comments and methodology

--- RectangleFunction.java
+++ RectangleFunction.java
@@ -25,11 +25,12 @@ public class RectangleFunction {
         double x3 = input.nextDouble();
         System.out.println("Input y value of testing coordinate");
         double y3 = input.nextDouble();
+        // takes in inputs of bottom left point coordinate, top right point coordinate and point to be tested
         
-        System.out.println(checkpoint(x1, y1, x2, y2, x3, y3));
-        System.out.println("break");
-        System.out.println(isarectangle(x1, y1, x2, y2));
-        
+        System.out.println(checkpoint(x1, y1, x2, y2, x3, y3)); // checks if point (x3, y3) is in the rectangle made by (x1, y1) and (x2, y2)
+        System.out.println("break"); // outputs break between outputs
+        System.out.println(isarectangle(x1, y1, x2, y2)); // checks if (x1, y1) and (x2, y2) make a valid rectangle
+        // outputs of variables are booleans: true or false
     }
     
     public static boolean checkpoint(double x1, double y1, double x2, double y2, double x3, double y3) {
@@ -38,6 +39,12 @@ public class RectangleFunction {
         } else {
             return false;
         }
+        
+        /* checkpoint checks whether the point to be tested (x3, y3) is in the rectangle made by (x1, y1) and (x2, y2)
+        if the point's x-value is in between the x-values of the rectangle's vertices and
+        the point's y-value is in between the y-values of the rectangle's vertices,
+        then the point is in the rectangle.
+        */
     }
     
     public static boolean isarectangle(double x1, double y1, double x2, double y2) {
@@ -46,6 +53,12 @@ public class RectangleFunction {
         } else {
             return true;
         }
+        /*
+        isarectangle checks whether the rectangle made by points (x1, x2) and (y1, y2) is, in fact, a rectangle.
+        since the program is given that (x1, y1) will be the bottom left vertice and (x2, y2) will by the top right vertice,
+        any set of points will create a rectangle, unless the either the x-values or the y-values of the vertices are equal.
+        if the vertices are equal, the vertices do not make a rectangle
+        */
     }
     
 }
