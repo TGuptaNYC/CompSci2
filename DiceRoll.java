0f1c692f43dbed6fa5aa440d30167b05ce410fc6
TGuptaNYC <tguptanyc@gmail.com>
2015-11-17 10:07:49 -0500

    Create DiceRoll.java

---
+++ DiceRoll.java
@@ -0,0 +1,38 @@
+package diceroll;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class DiceRoll {
+
+    public static void main(String[] args) {
+        int[] counter = new int[13];
+        for (int i = 0; i < 1000; i++) {
+            counter[diceaddition(2)]++;
+        }
+    System.out.println("2 = " + counter[2]);
+    System.out.println("3 = " + counter[3]);
+    System.out.println("4 = " + counter[4]);
+    System.out.println("5 = " + counter[5]);
+    System.out.println("6 = " + counter[6]);
+    System.out.println("7 = " + counter[7]);
+    System.out.println("8 = " + counter[8]);
+    System.out.println("9 = " + counter[9]);
+    System.out.println("10 = " + counter[10]);
+    System.out.println("11 = " + counter[11]);
+    System.out.println("12 = " + counter[12]);
+    }
+    
+    public static int diceaddition(int a) {
+        int[] dices = new int[a];
+        int sum = 0;
+        for (int i = 0; i < a; i++) {
+            dices[i] = (int)(Math.random()*6 + 1);
+            sum += dices[i];
+        }
+        return sum;
+    }
+    
+    
+}
